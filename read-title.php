c5c7fd48555c1cbb39d2aa3bbc6fd45ee2ac1bf0
alexandresalome <alexandre.salome@gmail.com>
2012-01-10 22:12:35 +0100

    Initial commit

---
+++ read-title.php
@@ -0,0 +1,45 @@
+<?php
+require_once __DIR__.'/vendor/.composer/autoload.php';
+
+$dataDir = __DIR__.'/cache';
+
+if (!is_dir($dataDir)) {
+    mkdir($dataDir);
+}
+
+if (!isset($argv[1])) {
+    echo "Usage: php read-title.php mon-url\n";
+    exit;
+}
+
+$url = $argv[1];
+$hash = md5($url);
+
+$file = $dataDir.'/'.md5($hash);
+
+if (file_exists($file)) {
+    list($statusCode, $content) = unserialize(file_get_contents($file));
+} else {
+    $statusCode = $content = null;
+}
+
+$browser = new Buzz\Browser();
+
+$browser->call($url, 'GET');
+$response = $browser->getLastResponse();
+
+$newStatusCode = $response->getStatusCode();
+$newContent    = $response->getContent();
+
+if ($statusCode !== null && $content !== null) {
+    if ($newStatusCode != $statusCode || $newContent != $content) {
+        echo 'The page has changed !'."\n";
+        echo 'Old status code: '.$statusCode."\n";
+        echo 'New status code: '.$newStatusCode."\n";
+        echo '------'."\n";
+        echo $newContent;
+        exec("notify-send 'FREE!!! FREE!!! FREE!!! FREE!!! FREE!!! FREE!!! FREE!!! FREE!!! FREE!!! FREE!!!'");
+    }
+}
+
+file_put_contents($file, serialize(array($newStatusCode, $newContent)));
